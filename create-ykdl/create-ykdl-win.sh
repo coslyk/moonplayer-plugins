bf4abac6ccdc372663030eb2546c04997ac83af4
coslyk <cos.lyk@gmail.com>
2019-12-28 20:22:07 +0100

    Add windows build

---
+++ create-ykdl/create-ykdl-win.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+HERE="$(dirname "$(readlink -f "${0}")")"
+
+# Download latest ykdl code
+if [ -d ykdl ]; then
+    rm -rf ykdl
+fi
+git clone 'https://github.com/zhangn1985/ykdl.git'
+
+# Run PyInstaller
+cp "${HERE}/ykdl_main.py" ykdl/ykdl-moonplayer.py
+cd ykdl
+pyinstaller -F --distpath .. ykdl-moonplayer.py
+cd ..
