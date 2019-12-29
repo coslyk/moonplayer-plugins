dfe8683f88ce5794d4ffd41c05e2704e725214c2
YK Liu <cos.lyk@gmail.com>
2019-12-30 00:15:38 +0100

    plugins: update bilibili

--- plugins/bilibili.js
+++ plugins/bilibili.js
@@ -22,20 +22,13 @@ function search(key, page) {
         }
         var result = [];
         for (var i in items)
-            result.push({title: items[i].title, url: items[i].uri});
+            result.push({title: items[i].title, url: convert_to_album_url(items[i].uri)});
         moonplayer.show_result(result);
     });
 }
 
 
-// Load item
-/*
-var title_re = /"mediaInfo":{.*?"title":"(.+?)"/
-var summary_re = /"evaluate":"(.+?)"/
-var cover_re = /"cover":(".+?")/
-var srcs_re = /"epList":(\[.*?\])/
-
-function load_item(url) {
+function convert_to_album_url(url) {
     var season;
     if (url.startsWith('bilibili://bangumi/season/'))
         season = url.replace('bilibili://bangumi/season/', '');
@@ -43,54 +36,9 @@ function load_item(url) {
         season = url.replace('/bangumi/i/', '');
     else if (url.includes('/anime/'))
         season = url.split('/anime/')[1];
-    else {
-        moonplayer.warning('Bilibili: Cannot open url:' + url);
-        return;
-    }
+    else
+        return url;
     season = season.replace(/\/$/, '');
-    url = 'https://www.bilibili.com/bangumi/play/ss' + season;
-    moonplayer.get_content(url, function(content){
-        // Infos
-        var match;
-        if ((match = title_re.exec(content)) === null) {
-            moonplayer.warning('Bilibili: Fails to get bangumi info!');
-            return;
-        }
-        var title = match[1];
-        if ((match = summary_re.exec(content)) === null) {
-            moonplayer.warning('Bilibili: Fails to get bangumi info!');
-            return;
-        }
-        var summary = match[1];
-        if ((match = cover_re.exec(content)) === null) {
-            moonplayer.warning('Bilibili: Fails to get bangumi info!');
-            return;
-        }
-        var cover = JSON.parse(match[1]);
-        if (!cover.startsWith('http'))
-            cover = 'http:' + cover;
-        // Urls
-        if ((match = srcs_re.exec(content)) === null) {
-            moonplayer.warning('Bilibili: Parsing fails!');
-            return;
-        }
-        var items = JSON.parse(match[1]);
-        var srcs = [];
-        for (i in items) {
-            var name = "[" + items[i].title + "] " + items[i].longTitle;
-            var url = 'https://www.bilibili.com/bangumi/play/ep' + items[i].id;
-            srcs.push(name);
-            srcs.push(url);
-        }
-        
-        // Result
-        var result = {
-            name: title,
-            image: cover,
-            summary: summary,
-            source: srcs
-        };
-        moonplayer.show_detail(result);
-    });
-}*/
+    return 'https://www.bilibili.com/bangumi/play/ss' + season;
+}
 
